8ae2ec7ba5e27916db289f82c10687f246d40990
Eva Bacas <ENB30@pitt.edu>
2021-10-12 10:13:24 -0400

    first commit

---
+++ brainwarp.sh
@@ -0,0 +1,30 @@
+echo "loading filename and optional parameters..."
+t1=`jq -r '.t1' config.json`
+optional_params=`jq -r '.optional_params' config.json` # currently the only option is null
+intense=`jq -r '.intense' config.json` # true, false, or test
+
+echo "loading template..."
+template=`jq -r '.template' config.json` # currently the only option is MNI152
+prefix=output/t1w_
+
+echo "creating output folder..."
+mkdir -p output
+
+echo "warping..."
+
+if [ "$intense" == "false" ]; then
+    # less intense
+    ANTS 3 -m CC[$t1,$template,1,4] -i 100x100x50 -o $prefix -t SyN[0.25] -r Gauss[3,0]
+elif [ "$intense" == "true" ]; then
+    # more intense
+    ANTS 3 -m CC[$t1,$template,1,4] -i 100x100x100x25 -o $prefix -t SyN[0.25] -r Gauss[3,0]
+elif [ "$intense" == "test" ]; then
+    # for testing, only iterates 1x1x1
+    ANTS 3 -m CC[$t1,$template,1,4] -i 1x1x1 -o $prefix -t SyN[0.25] -r Gauss[3,0]
+else
+    # error out
+    echo "how did you avoid setting a value for intense??"
+    exit 1
+fi
+
+echo "warp complete!"
